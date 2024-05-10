de05cd1146180af7a65a2cf246caabea115d40df
fedemelo <f.melo@uniandes.edu.co>
2024-05-10 18:46:01 -0500

    Updates

---
+++ assets/InlineDefinition-DUL1-8WO.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-yWy88T1L.js";import{g as r}from"./Definition-CQNBlEz8.js";function a({children:s,concept:e,id:n}){return n=n||r(e),i.jsxs("div",{className:"definition-container",children:[i.jsx("p",{children:s}),i.jsx("div",{className:"concept-side",id:n,children:i.jsx("strong",{children:e})})]})}export{a as I};
--- assets/components/notes/environments/Example.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import './BoxStyles.css';
-import './ExampleStyles.css';
-const Example = ({ children }) => (_jsx("div", { className: "example-box", children: children }));
-Example.Title = ({ children }) => (_jsxs("p", { className: "example-title", children: ["Ejemplo: ", children] }));
-export default Example;
