49a95b91a5ebbd0bd6931fd3d2f5d1695f8aed37
fedemelo <f.melo@uniandes.edu.co>
2024-05-10 19:09:13 -0500

    Refactor routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,69 +4,49 @@ import { Route, Routes } from "react-router-dom";
 import Loader from "@/components/loader/Loader";
 
 const Home = React.lazy(() => import("@/pages/Home"));
+
 const CalculoDiferencial = React.lazy(() => import("@/apuntes/matematica/CalculoDiferencial"));
 const AlgebraLineal = React.lazy(() => import("@/apuntes/matematica/AlgebraLineal"));
-const Notacion  = React.lazy(() => import("@/apuntes/matematica/Notacion"));
+const Notacion = React.lazy(() => import("@/apuntes/matematica/Notacion"));
 const TeoriaDeGrafos = React.lazy(() => import("@/apuntes/matematica/TeoriaDeGrafos"));
 const TeoriaDeJuegos = React.lazy(() => import("@/apuntes/matematica/TeoriaDeJuegos"));
 
+const notes = [
+  [CalculoDiferencial, "calculo_diferencial"],
+  [AlgebraLineal, "algebra_lineal"],
+  [Notacion, "notacion"],
+  [TeoriaDeGrafos, "teoria_de_grafos"],
+  [TeoriaDeJuegos, "teoria_de_juegos"],
+];
+
 export default function Router() {
-    return (
-        <Routes>
-            <Route
-                key="home"
-                path="/"    
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <Home />
-                    </React.Suspense>
-                }
-            />
-            <Route
-                key="apuntes"
-                path="/apuntes/matematica/calculo_diferencial"
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <CalculoDiferencial />
-                    </React.Suspense>
-                }
-            />
-            <Route
-                key="apuntes"
-                path="/apuntes/matematica/algebra_lineal"
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <AlgebraLineal />
-                    </React.Suspense>
-                }
-            />
-            <Route
-                key="apuntes"
-                path="/apuntes/matematica/notacion"
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <Notacion />
-                    </React.Suspense>
-                }
-            />
-            <Route
-                key="apuntes"
-                path="/apuntes/matematica/teoria_de_grafos"
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <TeoriaDeGrafos />
-                    </React.Suspense>
-                }
-            />
-            <Route
-                key="apuntes"
-                path="/apuntes/matematica/teoria_de_juegos"
-                element={
-                    <React.Suspense fallback={<Loader />}>
-                        <TeoriaDeJuegos />
-                    </React.Suspense>
-                }
-            />
-        </Routes>
-    );
+  console.log(CalculoDiferencial.name);
+
+  return (
+    <Routes>
+      <Route
+        key="home"
+        path="/"
+        element={
+          <React.Suspense fallback={<Loader />}>
+            <Home />
+          </React.Suspense>
+        }
+      />
+      {notes.map((note, index) => {
+        const Note = note[0];
+        return (
+          <Route
+            key={index}
+            path={`/apuntes/matematica/${note[1]}`}
+            element={
+              <React.Suspense fallback={<Loader />}>
+                <Note />
+              </React.Suspense>
+            }
+          />
+        );
+      })}
+    </Routes>
+  );
 }
