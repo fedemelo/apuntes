31cff36b7004544603ef95e91a6d92653b0ed4b1
fedemelo <federicomelobarrero@gmail.com>
2024-05-11 18:06:37 +0000

    Updates

---
+++ assets/InlineDefinition-CNFPAZG8.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-rpnlq0L5.js";import{g as r,B as t}from"./NoteDocument-vQ199TAK.js";function x({children:s,concept:e,id:i}){return i=i||r(e),n.jsxs(t,{children:[n.jsx("p",{children:s}),n.jsx("div",{className:"definition-concept-side",id:i,children:n.jsx("strong",{children:e})})]})}export{x as I};
