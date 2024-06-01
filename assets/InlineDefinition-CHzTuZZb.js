3fe4ebdeb46a471c6fdc33618078cc7333835f41
fedemelo <federicomelobarrero@gmail.com>
2024-06-01 04:21:03 +0000

    Updates

---
+++ assets/InlineDefinition-CHzTuZZb.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-D2sQO4YO.js";import{g as r,B as t}from"./NoteDocument-Du_k2b8U.js";function x({children:s,concept:e,id:i}){return i=i||r(e),n.jsxs(t,{children:[n.jsx("p",{children:s}),n.jsx("div",{className:"definition-concept-side",id:i,children:n.jsx("strong",{children:e})})]})}export{x as I};
