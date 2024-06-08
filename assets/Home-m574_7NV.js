557489bf3dcc4fd61cebd1608579dd2e942ee216
fedemelo <federicomelobarrero@gmail.com>
2024-06-08 21:04:11 +0000

    Updates

--- assets/Home-m574_7NV.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DIhf27_q.js";function o(){return e.jsx("div",{children:e.jsx("h1",{children:"Home"})})}export{o as default};
---
+++ assets/InlineDefinition-DrsDsB2f.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-ChcYV5Lm.js";import{g as r,B as t}from"./NoteDocument-CG_Et5TW.js";function x({children:s,concept:e,id:i}){return i=i||r(e),n.jsxs(t,{children:[n.jsx("p",{children:s}),n.jsx("div",{className:"definition-concept-side",id:i,children:n.jsx("strong",{children:e})})]})}export{x as I};
