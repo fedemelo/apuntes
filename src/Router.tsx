743edaf30c6245e8f0760ebf3b58f114bc8f3845
fedemelo <f.melo@uniandes.edu.co>
2024-06-14 19:53:12 -0500

    Add support to references between files

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,35 +5,50 @@ import Loader from "@/components/loader/Loader";
 
 
 const Custom404 = React.lazy(() => import("@/pages/Custom404"));
-const mathUrl = "/apuntes/matematica/";
+
+const baseUrl = "apuntes";
+const math = "matematica";
+const differentialCalculus = "calculo-diferencial";
+const vectorCalculus = "calculo-vectorial";
+const linearAlgebra = "algebra-lineal";
+const notation = "notacion";
+const graphTheory = "teoria-de-grafos";
+const gameTheory = "teoria-de-juegos";
+export const DIFFERENTIAL_CALCULUS = `/${baseUrl}/${math}/${differentialCalculus}`;
+export const VECTOR_CALCULUS = `/${baseUrl}/${math}/${vectorCalculus}`;
+export const LINEAR_ALGEBRA = `/${baseUrl}/${math}/${linearAlgebra}`;
+export const NOTATION = `/${baseUrl}/${math}/${notation}`;
+export const GRAPH_THEORY = `/${baseUrl}/${math}/${graphTheory}`;
+export const GAME_THEORY = `/${baseUrl}/${math}/${gameTheory}`;
+
 const files = [
     {
         Component: React.lazy(() => import("@/pages/Home")), 
         path: "/apuntes/"
     },
     {
-        Component: React.lazy(() => import("@/apuntes/matematica/calculo-diferencial/CalculoDiferencial")), 
-        path: `${mathUrl}calculo-diferencial`
+        Component: React.lazy(() => import("@/apuntes/matematica/calculo-diferencial/CalculoDiferencial")),
+        path: DIFFERENTIAL_CALCULUS
     },
     {
         Component: React.lazy(() => import("@/apuntes/matematica/calculo-vectorial/CalculoVectorial")),
-        path: `${mathUrl}calculo-vectorial`
+        path: VECTOR_CALCULUS
     },
     {
-        Component: React.lazy(() => import("@/apuntes/matematica/algebra-lineal/AlgebraLineal")), 
-        path: `${mathUrl}algebra-lineal`
+        Component: React.lazy(() => import("@/apuntes/matematica/algebra-lineal/AlgebraLineal")),
+        path: LINEAR_ALGEBRA
     },
     {
-        Component: React.lazy(() => import("@/apuntes/matematica/notacion/Notacion")), 
-        path: `${mathUrl}notacion`
+        Component: React.lazy(() => import("@/apuntes/matematica/notacion/Notacion")),
+        path: NOTATION
     },
     {
-        Component: React.lazy(() => import("@/apuntes/matematica/teoria-de-grafos/TeoriaDeGrafos")), 
-        path: `${mathUrl}teoria-de-grafos`
+        Component: React.lazy(() => import("@/apuntes/matematica/teoria-de-grafos/TeoriaDeGrafos")),
+        path: GRAPH_THEORY
     },
     {
-        Component: React.lazy(() => import("@/apuntes/matematica/teoria-de-juegos/TeoriaDeJuegos")), 
-        path: `${mathUrl}teoria-de-juegos`
+        Component: React.lazy(() => import("@/apuntes/matematica/teoria-de-juegos/TeoriaDeJuegos")),
+        path: GAME_THEORY
     }
 ]
 
