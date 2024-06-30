8335569cc0a48bc426859beb74dee32bb37c11ca
fedemelo <f.melo@uniandes.edu.co>
2024-06-29 20:02:30 -0500

    Add TODO for series and parametric curves and stop studying differential calculus -for now

--- src/apuntes/matematica/calculo-integral/CalculoIntegral.tsx
+++ src/apuntes/matematica/calculo-integral/CalculoIntegral.tsx
@@ -1,14 +1,13 @@
-import { Definition, Example, Theorem } from "@/components/notes/environments/Boxes";
+import { Definition, Theorem } from "@/components/notes/environments/Boxes";
 import Proof from "@/components/notes/environments/Proof";
 import Properties from "@/components/notes/environments/Properties";
-import { Notation, Tip, Warning } from "@/components/notes/environments/StickyNotes";
+import { Notation, Tip } from "@/components/notes/environments/StickyNotes";
 import Epigraph from "@/components/notes/Epigraph";
 import NoteDocument from "@/components/notes/NoteDocument";
 import Section from "@/components/notes/Section";
-import H3 from "@/components/notes/Subsection";
 import { referenceById } from "@/components/utils/hyperlinkManager";
 import M, { r } from "@/math/LaTeX";
-import { DIFFERENTIAL_CALCULUS, INTEGRAL_CALCULUS, VECTOR_CALCULUS } from "@/Router";
+import { DIFFERENTIAL_CALCULUS, INTEGRAL_CALCULUS } from "@/Router";
 
 const CalculoIntegral = () => (
   <NoteDocument>
@@ -1017,6 +1016,8 @@ const CalculoIntegral = () => (
       </p>
 
     </Section>
+
+    {/* TODO: Agregar sucesiones, series, curvas paramétricas. */}
 
   </NoteDocument >
 );
