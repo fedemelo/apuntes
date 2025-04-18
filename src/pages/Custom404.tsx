5c229c949c8d9999e6efc63a5361e8f5a09e5ab5
Federico Melo <f.melo@uniandes.edu.co>
2025-04-18 14:50:19 -0500

    Beautify Custom404 page

--- src/pages/Custom404.tsx
+++ src/pages/Custom404.tsx
@@ -1,16 +1,33 @@
+import { Typography, Container, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 export default function Custom404() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1 style={{ fontSize: "6em" }}>404</h1>
-      <h5 style={{ fontSize: "2em" }}>No los encontré :(</h5>
-      <p style={{ fontSize: "1.5em" }}>
+    <Container maxWidth="md" sx={{ textAlign: 'center', margin: 8 }} >
+      <Typography variant="h1" sx={{ fontSize: '6rem', fontWeight: 'bold' }}>
+        404
+      </Typography>
+      <Typography variant="h5" sx={{ fontSize: '2rem' }}>
+        No los encontré :(
+      </Typography>
+      <Typography variant="body1" sx={{ fontSize: '1rem', my: 4 }}>
         Lo lamento, no tengo apuntes sobre este tema todavía.
-        <p />
-        <p>Puede que no los haya subido aún o simplemente que jamás haya cursado una materia sobre esto.</p>
-        <p>
-          Iré subiendo los apuntes de todos mis cursos a medida que pueda revisarlos y completarlos. Algunos están más depurados que otros y estarán disponibles para todos más pronto, pero tarde o temprano todos estarán en esta página!
-        </p>
-      </p>
-    </div>
+      </Typography>
+      <Typography variant="body1" sx={{ fontSize: '1rem', mb: 4 }}>
+        Puede que no los haya subido aún o simplemente que jamás haya estudiado esto.
+      </Typography>
+      <Typography variant="body1" sx={{ fontSize: '1rem', mb: 4 }}>
+        Iré subiendo todos mis apuntes a medida que pueda revisarlos y completarlos. Algunos están más depurados que otros y estarán disponibles para todos más pronto, pero tarde o temprano todos estarán en esta página!
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to="/apuntes/"
+        sx={{ mt: 4 }}
+      >
+        Volver al inicio
+      </Button>
+    </Container>
   );
 }
