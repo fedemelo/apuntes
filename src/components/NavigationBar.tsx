4bc59f05cc96c2b7c6d471fa3e47d64086475bc4
Federico Melo <f.melo@uniandes.edu.co>
2025-04-18 14:06:02 -0500

    Fix navbar formatting

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -14,104 +14,104 @@ import HomeIcon from "@mui/icons-material/Home";
 import existingNotes from "@/apuntes/existingNotes.json";
 
 export default function NavigationBar() {
-    const [openMenuId, setOpenMenuId] = React.useState<string | null>(null);
+  const [openMenuId, setOpenMenuId] = React.useState<string | null>(null);
 
-    const handleClick = (categoryId: string) => () => {
-        if (openMenuId === categoryId) {
-            setOpenMenuId(null);
-        } else {
-            setOpenMenuId(categoryId);
-        }
-    };
+  const handleClick = (categoryId: string) => () => {
+    if (openMenuId === categoryId) {
+      setOpenMenuId(null);
+    } else {
+      setOpenMenuId(categoryId);
+    }
+  };
 
-    const handleClose = () => {
-        setOpenMenuId(null);
-    };
+  const handleClose = () => {
+    setOpenMenuId(null);
+  };
 
-    const buttonStyles = {
-        fontSize: '1.1rem',
-        marginLeft: '17px',
-        ':hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.1)',
-        },
-    };
+  const buttonStyles = {
+    fontSize: '1.1rem',
+    marginLeft: '17px',
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    },
+  };
 
-    const menuItemStyles = {
-        fontSize: '0.95rem',
-        ':hover': {
-            backgroundColor: 'rgba(255, 255, 255, 0.08)',
-        },
-    };
+  const menuItemStyles = {
+    fontSize: '0.95rem',
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.08)',
+    },
+  };
 
-    return (
-        <AppBar position="static" className="navigation-bar">
-            <Container>
-                <Toolbar disableGutters className="toolbar">
-                    <Button
+  return (
+    <AppBar position="static" className="navigation-bar">
+      <Container>
+        <Toolbar disableGutters className="toolbar">
+          <Button
+            component={Link}
+            to="/"
+            color="inherit"
+            // Remove marginLeft for the Home button, as extra margin
+            // is added for spacing between buttons
+            sx={{ ...buttonStyles, marginLeft: 0 }}
+            startIcon={<HomeIcon />}
+          >
+            Home
+          </Button>
+
+          {Object.entries(existingNotes).map(([topic, subtopics]) => {
+            const kebabCaseTopic = kebabCase(removeAccents(topic));
+            return (
+              <React.Fragment key={topic}>
+                <Button
+                  className="topic-button"
+                  id={`${kebabCaseTopic}-button`}
+                  aria-controls={openMenuId === kebabCaseTopic ? `${kebabCaseTopic}-menu` : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={openMenuId === kebabCaseTopic ? "true" : undefined}
+                  onClick={handleClick(kebabCaseTopic)}
+                  color="inherit"
+                  sx={buttonStyles}
+                >
+                  {topic}
+                </Button>
+                <Menu
+                  id={`${kebabCaseTopic}-menu`}
+                  anchorEl={document.getElementById(`${kebabCaseTopic}-button`)}
+                  open={openMenuId === kebabCaseTopic}
+                  onClose={handleClose}
+                  MenuListProps={{ "aria-labelledby": `${kebabCaseTopic}-button` }}
+                >
+                  {subtopics.map(({ name, description }: { name: string, description: string }) => {
+                    const kebabCaseSubtopic = kebabCase(removeAccents(name));
+
+                    const menuItem = (
+                      <MenuItem
+                        onClick={handleClose}
                         component={Link}
-                        to="/"
-                        color="inherit"
-                        // Remove marginLeft for the Home button, as extra margin
-                        // is added for spacing between buttons
-                        sx={{ ...buttonStyles, marginLeft: 0 }}
-                        startIcon={<HomeIcon />}
-                    >
-                        Home
-                    </Button>
+                        to={`/apuntes/${kebabCaseTopic}/${kebabCaseSubtopic}`}
+                        sx={menuItemStyles}
+                      >
+                        {name}
+                      </MenuItem>
+                    );
 
-                    {Object.entries(existingNotes).map(([topic, subtopics]) => {
-                        const kebabCaseTopic = kebabCase(removeAccents(topic));
-                        return (
-                            <React.Fragment key={topic}>
-                                <Button
-                                    className="topic-button"
-                                    id={`${kebabCaseTopic}-button`}
-                                    aria-controls={openMenuId === kebabCaseTopic ? `${kebabCaseTopic}-menu` : undefined}
-                                    aria-haspopup="true"
-                                    aria-expanded={openMenuId === kebabCaseTopic ? "true" : undefined}
-                                    onClick={handleClick(kebabCaseTopic)}
-                                    color="inherit"
-                                    sx={buttonStyles}
-                                >
-                                    {topic}
-                                </Button>
-                                <Menu
-                                    id={`${kebabCaseTopic}-menu`}
-                                    anchorEl={document.getElementById(`${kebabCaseTopic}-button`)}
-                                    open={openMenuId === kebabCaseTopic}
-                                    onClose={handleClose}
-                                    MenuListProps={{ "aria-labelledby": `${kebabCaseTopic}-button` }}
-                                >
-                                    {subtopics.map(({ name, description }: { name: string, description: string }) => {
-                                        const kebabCaseSubtopic = kebabCase(removeAccents(name));
-
-                                        const menuItem = (
-                                            <MenuItem
-                                                onClick={handleClose}
-                                                component={Link}
-                                                to={`/apuntes/${kebabCaseTopic}/${kebabCaseSubtopic}`}
-                                                sx={menuItemStyles}
-                                            >
-                                                {name}
-                                            </MenuItem>
-                                        );
-
-                                        return description ? (
-                                            <Tooltip key={kebabCaseSubtopic} title={description} arrow>
-                                                {menuItem}
-                                            </Tooltip>
-                                        ) : (
-                                            <React.Fragment key={kebabCaseSubtopic}>
-                                                {menuItem}
-                                            </React.Fragment>
-                                        );
-                                    })}
-                                </Menu>
-                            </React.Fragment>
-                        );
-                    })}
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+                    return description ? (
+                      <Tooltip key={kebabCaseSubtopic} title={description} arrow>
+                        {menuItem}
+                      </Tooltip>
+                    ) : (
+                      <React.Fragment key={kebabCaseSubtopic}>
+                        {menuItem}
+                      </React.Fragment>
+                    );
+                  })}
+                </Menu>
+              </React.Fragment>
+            );
+          })}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
